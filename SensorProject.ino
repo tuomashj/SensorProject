27a1157cf4965c1a28e37d023514aa9522173cfc
Tuomas Jama <tuomas.jama@metropolia.fi>
2026-03-17 22:34:52 +0200

    rubberduck

--- SensorProject.ino
+++ SensorProject.ino
@@ -1,15 +1,17 @@
 const int sampleInterval = 10;
-unsigned long previousMillis = 0;
-
-unsigned long lastPeak = 0;
+unsigned long prevSample = 0;
 
 int potValue = 0;
 
-long runningSum = 0;
+unsigned long runningSum = 0;
 float runningAvg = 0;
 int i = 0;
+int trigger = 0;
 
 int beats = 0;
+unsigned long lastBeatTime = 0;
+unsigned long beatInterval = 0;
+int BPM = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -20,25 +22,33 @@ void setup() {
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= sampleInterval) {
+  unsigned long currSample = millis();
+  if (currSample - prevSample >= sampleInterval) {
+    digitalWrite(6, LOW);
     potValue = analogRead(A1);
+    prevSample = currSample;
     runningSum += potValue;
     i++;
     if (i == 10) {
       runningAvg = runningSum / i;
-      Serial.println(runningSum);
-      Serial.println(runningAvg);
-      if (runningAvg >= 621) {
+      //Serial.println(runningSum);
+      if (runningAvg >= 20 && trigger == 0) {
         digitalWrite(6, HIGH);
-        lastPeak = currentMillis;
+        trigger = 1;
         beats++;
-      } else if (runningAvg < 621) {
-        digitalWrite(6, LOW);
+        beatInterval += currSample - lastBeatTime;
+        BPM = 60000 / (beatInterval/beats);
+        lastBeatTime = currSample;
+        if (beats >= 10) {
+          beatInterval = 0;
+          beats = 0;
+        }
+      } else if (runningAvg < 20) {
+        trigger = 0;
       }
-      i = 0;
+      Serial.println(BPM);
       runningSum = 0;
-      previousMillis = currentMillis;
+      i = 0;
     }
   }
   // put your main code here, to run repeatedly:
